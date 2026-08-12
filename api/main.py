4da5e7ae4d65e46eeb86a2936f757e43f9831d76
Fardeen NS Khan <fardeennskhan2003@gmail.com>
2026-08-12 09:34:44 +0000

    fix: add localhost:5173 to CORS for React frontend

--- api/main.py
+++ api/main.py
@@ -45,9 +45,9 @@ app = FastAPI(title="AskMyDocs", version="1.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174"],
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"],    
 )
 
 
